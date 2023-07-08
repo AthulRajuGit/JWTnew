49ec93b23ef3c7b18280a08ea45936810c099c3d
athraj <athul.raju.500@gmail.com>
2023-07-08 09:45:05 +0530

    first commit

--- src/main/java/com/springsec/example/config/SecurityConfig.java
+++ src/main/java/com/springsec/example/config/SecurityConfig.java
@@ -1,12 +1,18 @@
 package com.springsec.example.config;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -14,6 +20,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -25,8 +32,8 @@ public class SecurityConfig {
 	 public UserDetailsService userDetailsService() {
 		
 		//UserDetails admin=User.withUsername("athul").password(encoder.encode("psw1")).roles("ADMIN").build();
-		//UserDetails user=User.withUsername("Joseph").password(encoder.encode("psw2")).roles("USER").build();
-	//  return new InMemoryUserDetailsManager(admin,user);
+	    //UserDetails user=User.withUsername("Joseph").password(encoder.encode("psw2")).roles("USER").build();
+	    //return new InMemoryUserDetailsManager(admin,user);
 		
 		return new UserInfoUserDetailsService();
 	}
@@ -34,7 +41,7 @@ public class SecurityConfig {
 	
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity sec) throws Exception{
-	return	sec.csrf().disable().authorizeHttpRequests().requestMatchers("/products/welcome","/products/new").permitAll()
+	return	sec.csrf().disable().authorizeHttpRequests().requestMatchers("/products/new","/products/auth").permitAll()
 			.and().authorizeHttpRequests()
 		    .requestMatchers("/products/**")
 		    .authenticated().and().formLogin().and().build();
@@ -52,4 +59,10 @@ public class SecurityConfig {
 		authentication.setUserDetailsService(userDetailsService());
 		return authentication;
 	}
+	
+	
+	@Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
 }
--- src/main/java/com/springsec/example/controller/ProductContoller.java
+++ src/main/java/com/springsec/example/controller/ProductContoller.java
@@ -2,9 +2,14 @@ package com.springsec.example.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,19 +19,27 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springsec.example.Entity.User;
+import com.springsec.example.dto.AuthReq;
 import com.springsec.example.dto.Products;
+import com.springsec.example.service.JwtService;
 import com.springsec.example.service.UserService;
 
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/products")
-@RequiredArgsConstructor
 public class ProductContoller {
 	
-	private final UserService userService;
+	@Autowired
+	private  UserService userService;
 	
-
+	@Autowired
+    private  AuthenticationManager authenticationManager;
+    
+	@Autowired
+    private  JwtService jwtService;
+	
+	
 	@GetMapping("/welcome")
 	public String welcome() {
 		return "this endpoint is not welcome";
@@ -53,4 +66,15 @@ public class ProductContoller {
 		
 	}
 	
+	@PostMapping("/auth")
+    public String authenticateAndGetToken(@RequestBody AuthReq authReq) {
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authReq.getUsername(), authReq.getPassword()));
+        if (authentication.isAuthenticated()) {
+            return jwtService.generateToken(authReq.getUsername());
+        } else {
+            throw new UsernameNotFoundException("invalid user request !");
+        }
+
+
+    }
 }
